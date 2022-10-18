3fb2a6795148a78f1635b17f468e396232f9d124
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2022-10-18 11:58:45 +0300

    Display codehash in "artifacts.json".

--- build_within_docker.py
+++ build_within_docker.py
@@ -35,11 +35,14 @@ class BuildArtifactsAccumulator:
         self.contracts: Dict[str, Dict[str, str]] = dict()
 
     def gather_artifacts(self, contract_name: str, output_subdirectory: Path):
+        with open(find_file_in_folder(output_subdirectory, "*.codehash.txt")) as file:
+            code_hash = file.read()
+
         self.add_artifact(contract_name, "bytecode", find_file_in_folder(output_subdirectory, "*.wasm").name)
         self.add_artifact(contract_name, "text", find_file_in_folder(output_subdirectory, "*.wat").name)
         self.add_artifact(contract_name, "abi", find_file_in_folder(output_subdirectory, "*.abi.json").name)
         self.add_artifact(contract_name, "imports", find_file_in_folder(output_subdirectory, "*.imports.json").name)
-        self.add_artifact(contract_name, "codehash", find_file_in_folder(output_subdirectory, "*.codehash.txt").name)
+        self.add_artifact(contract_name, "codehash", code_hash)
         self.add_artifact(contract_name, "src", find_file_in_folder(output_subdirectory, "*.zip").name)
 
     def add_artifact(self, contract_name: str, kind: str, value: str):
