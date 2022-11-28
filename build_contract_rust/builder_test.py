acc95e950b78688711dad02603bd01e613de3b82
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2022-11-28 09:43:55 +0200

    Unit testing.

--- build_contract_rust/builder_test.py
+++ build_contract_rust/builder_test.py
@@ -5,12 +5,29 @@ from build_contract_rust import builder
 
 def test_build_project():
     outcome = builder.build_project(
-        project_path=Path("./testdata/adder"),
+        project_path=Path("./testdata/input"),
         parent_output_directory=Path("./testdata/output"),
         specific_contract=None,
         cargo_target_dir=Path("/tmp/cargo-target-dir"),
         no_wasm_opt=False
     )
 
+    actual_source = outcome.get_entry("adder").artifacts.src_package.read()
+    actual_wat = outcome.get_entry("adder").artifacts.text.read()
+
     assert outcome.get_entry("adder").version == "1.2.3"
     assert outcome.get_entry("adder").codehash == "58c6e78f40bd6ccc30d8a01f952b34a13ebfdad796a2526678be17c5d7820174"
+    assert actual_source == read_binary_file(Path("./testdata/expected/adder-1.2.3.source.json"))
+    assert actual_wat == read_binary_file(Path("./testdata/expected/adder.wat"))
+
+    actual_source = outcome.get_entry("empty").artifacts.src_package.read()
+    actual_wat = outcome.get_entry("empty").artifacts.text.read()
+    assert outcome.get_entry("empty").version == "4.5.6"
+    assert outcome.get_entry("empty").codehash == "20df405fa1733a22748c888f6c1571f2c12cc40a8b1de800e0fd105674b426a5"
+    assert actual_source == read_binary_file(Path("./testdata/expected/empty-4.5.6.source.json"))
+    assert actual_wat == read_binary_file(Path("./testdata/expected/empty.wat"))
+
+
+def read_binary_file(path: Path) -> bytes:
+    with open(path, "rb") as f:
+        return f.read()
