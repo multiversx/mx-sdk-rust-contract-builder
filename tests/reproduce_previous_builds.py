374f322e75bf4f1086c127d0458086dfb6dcbd11
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2023-01-22 01:03:50 +0200

    Add extra tests (past builds to reproduce).

--- tests/reproduce_previous_builds.py
+++ tests/reproduce_previous_builds.py
@@ -131,6 +131,53 @@ builds: List[PreviousBuild] = [
             "token-unstake": "463e49892f64726450d0df5ab4ba26559ad882525ce5e93173a26fde8437266e",
         },
         docker_image="multiversx/sdk-rust-contract-builder:v3.1.0"
+    ),
+    PreviousBuild(
+        name="e.2",
+        project_zip_url="https://github.com/multiversx/mx-exchange-sc/archive/refs/heads/reproducible-v2.0-rc6.zip",
+        project_path_adjustment="mx-exchange-sc-reproducible-v2.0-rc6",
+        packaged_src_url=None,
+        contract_name=None,
+        expected_code_hashes={
+            "distribution": "17a30ad44291af84f6dbd84fdaf0a9a56ed7145d544c54fd74088bb544c4f98f",
+            "energy-factory": "62d60c8dec649614dd9cf04fb20b884c7658b12759fa14bf7e9c7be3880a5edd",
+            "energy-factory-mock": "83b2f26a52e3fe74953f2a8cfd81f169664a4e59dae4e5d5bb1d89956fd81d43",
+            "energy-update": "8523bf84ac56626c70c31342487445bf8123e3ef5f906dcb39e8b5f16c4145b7",
+            "factory": "df06465b651594605466e817bfe9d8d7c68eef0f87df4a8d3266bcfb1bef6d83",
+            "farm": "69f95b5f9a4d5b6bb101d5d2cf7495264a4d04de2b36653e0c8088cf6fad492a",
+            "farm-staking": "ca0a8ceed8b8807b0fb078153c15167a3a235a61a76edc5023dfcacae0446125",
+            "farm-staking-proxy": "56468a6ae726693a71edcf96cf44673466dd980412388e1e4b073a0b4ee592d7",
+            "farm-with-locked-rewards": "c18d75ea788ece457788ad8849722a42dd4a12e6e23ab87f0cdffcc0116b61be",
+            "fees-collector": "c46767232cd8551f8b0f4aa94dc419ddefc13eaaa5aa4b422749a300621149f3",
+            "governance": "959388eadaf71ff106252c601ae2767a5c62d7bd0ab119381c28dc679975685e",
+            "governance-v2": "786a6cf08f1d961814ebb062f149c9a943d39d7db93d8f53aa1fc42b8e652f49",
+            "lkmex-transfer": "995311e0dbd75ddc51a5c0c71ab896245c996b9b3993d3118a153bfb5531e123",
+            "locked-token-wrapper": "f9ee63d96163e3fac52a164c76d91c85fd77968393a50d4a96a7080e648d0a6c",
+            "metabonding-staking": "f508c5643b3d5f5e79b68762a9ca9e247c753acd305a29009328c5ec5d153bdd",
+            "pair": "23ce1e8910c105410b4a417153e4b38c550ab78b38b899ea786f0c78500caf21",
+            "pair-mock": "a54495375db964cf924391433605d602940174d4d28111b89b8689564d90e662",
+            "pause-all": "2ad8aa911555b41e397541eb46cd1a7fa87186146f8c2b295e3916303833f3cd",
+            "price-discovery": "6df095b15272b189c2e7b3628a21e17c1a6b26e5ed03e9a7bddac61be29d162f",
+            "proxy-deployer": "5108e7419546872d235f0b7db5e01c5d04fec243bfa599c666629ead13bab0aa",
+            "proxy_dex": "988dd8b632e1b4bb9b43e5636ef4c363dd4066186f64f6f783f9cd043aa906c1",
+            "router": "8429d332fb62b557b3549d3f509a55d6aff8638f53a5ee876358a831107102cf",
+            "simple-lock": "303290b7a08b091c29315dd6979c1f745fc05467467d7de64e252592074890a7",
+            "simple-lock-whitelist": "c576c6106234e5f7978efb1885afe36c5d6da6a13c12b459fd7fe95967646d13",
+            "token-unstake": "463e49892f64726450d0df5ab4ba26559ad882525ce5e93173a26fde8437266e",
+        },
+        docker_image="multiversx/sdk-rust-contract-builder:v3.1.0"
+    ),
+    PreviousBuild(
+        name="f.1",
+        project_zip_url="https://github.com/multiversx/mx-nft-marketplace-sc/archive/refs/heads/reproducible-v2.0.1.zip",
+        project_path_adjustment="mx-nft-marketplace-sc-reproducible-v2.0.1",
+        packaged_src_url=None,
+        contract_name=None,
+        expected_code_hashes={
+            "esdt-nft-marketplace": "aed8f014c914d2910cbb68b61adb757f8dbc8385842e717127482e1a66828bbe",
+            "seller-contract-mock": "d3f42ae77ec60878ba62146a4209ef08a9400aecf083c96888ede316069985c0"
+        },
+        docker_image="multiversx/sdk-rust-contract-builder:v4.0.3"
     )
 ]
 
